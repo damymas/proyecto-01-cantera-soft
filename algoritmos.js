1d9b05d04b78e124bfd4dc865a16500ef625f4c7
damyd <damydamie@gmail.com>
2022-08-11 20:29:24 -0300

    4funcionesyvariableshechas

---
+++ algoritmos.js
@@ -0,0 +1,39 @@
+function suma() {
+let x =10;
+let y =5;
+
+let suma = x + y;
+
+return console.log(suma); 
+
+}
+suma()
+
+function rest() {
+let a =6;
+let b =3;
+
+let rest = a-b;
+return console.log(rest)
+
+}
+rest()
+
+function multi() {
+let c =8;
+let d =2;
+    
+let multi = c*2;
+return console.log(multi)
+
+}
+multi()
+
+function division(){
+let r=7;
+let t=2;
+
+let division = r/t;
+return console.log(division)
+}
+division ()
