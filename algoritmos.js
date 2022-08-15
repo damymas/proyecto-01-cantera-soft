1e5f7ee392539d49254949f981fd6975b64cbe29
damyd <damydamie@gmail.com>
2022-08-15 18:49:03 -0300

    funcionesyoperacion

--- algoritmos.js
+++ algoritmos.js
@@ -36,4 +36,28 @@ let t=2;
 let division = r/t;
 return console.log(division)
 }
-division ()
+division ()[
+
+function suma2(){ 
+let q = 10;
+let w = 3;
+
+let suma2 = q+w;
+return console.log (suma2)
+}
+suma2 ()
+
+let a= 6;
+let b= 9;
+let resultado = a+ b;
+
+if (resultado > 10) {
+    function permiso (){ 
+        return console.log ("si");
+    }
+    permiso ();
+} else {
+    console.log("no");
+    
+}
+
